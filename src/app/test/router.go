2c69eb99e548a5c3decf15febd3366408650e50f
唐道勇 <matias@tangdaoongdeMBP.lan>
2022-04-19 18:49:49 +0800

    feat: - 添加默认接口

--- src/app/test/router.go
+++ src/app/test/router.go
@@ -2,8 +2,8 @@
  * @Author: matiastang
  * @Date: 2022-04-18 17:01:55
  * @LastEditors: matiastang
- * @LastEditTime: 2022-04-18 17:04:11
- * @FilePath: /matias-Golang/learn/src/app/get/router.go
+ * @LastEditTime: 2022-04-19 18:35:34
+ * @FilePath: /golang-scaffold/src/app/test/router.go
  * @Description: router
  */
 package test
@@ -11,5 +11,6 @@ package test
 import "github.com/gin-gonic/gin"
 
 func Routers(e *gin.Engine) {
+	e.GET("/", Index)
 	e.GET("/userinfo/:id", GetUserInfo)
 }
--- src/app/test/test.go
+++ src/app/test/test.go
@@ -2,8 +2,8 @@
  * @Author: matiastang
  * @Date: 2022-04-19 17:20:54
  * @LastEditors: matiastang
- * @LastEditTime: 2022-04-19 17:26:33
- * @FilePath: /golang-scaffold/app/test/test.go
+ * @LastEditTime: 2022-04-19 18:35:11
+ * @FilePath: /golang-scaffold/src/app/test/test.go
  * @Description: 测试
  */
 package test
@@ -14,6 +14,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+func Index(c *gin.Context) {
+	c.String(http.StatusOK, "ok")
+}
 
 type UserInfo struct {
 	Id   string `json:"userId"`
